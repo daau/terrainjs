a4d3cb1f6ab1f75bfb19c98da1604c16b9d91576
Daniel Au <dakmau@gmail.com>
2017-08-13 23:00:47 -0700

    Working midpoint generation

--- script.js
+++ script.js
@@ -1,15 +1,43 @@
 document.addEventListener("touchmove", function(event){
     event.preventDefault();
 })
-var c = document.getElementsByTagName('canvas')[0]
-x = c.getContext('2d')
-pr = window.devicePixelRatio || 1
-w = window.innerWidth
+var c = document.getElementsByTagName('canvas')[0],
+x = c.getContext('2d'),
+pr = window.devicePixelRatio || 1,
+w = window.innerWidth,
 h = window.innerHeight
-f = 90
-q
-m = Math
-r = 0
-u = m.PI*2
-v = m.cos
-z = m.random
+
+var minimumDistance = 10;
+
+class Point {
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+    }
+}
+
+var points = [];
+var a = new Point(0, 0);
+var b = new Point(100, 0);
+
+function generateMidpoint(a, b){
+    var midX = (a.x + b.x)/2;
+    var midY = (a.y + b.y)/2;
+    var mid = new Point(midX, midY);
+    
+    var dist = mid.x - a.x;
+    if (dist <= minimumDistance){
+        points.push(a);
+        return;
+    }
+
+    generateMidpoint(a, mid);
+    generateMidpoint(mid, b);
+
+}
+
+function midpointDisplacement(a, b){
+    generateMidpoint(a, b);
+    points.push(b);
+}
+
