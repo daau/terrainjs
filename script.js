492c88cc0dbfa63bed877775bf133455511aa479
Daniel Au <dakmau@gmail.com>
2019-09-01 19:34:36 -0700

    Remove old legacy code

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-const MIN_X = 1,
-MIN_Y = 1000,
-DECAY = 10,
-DECAY_RATE = 20;
-
-var c = document.getElementsByTagName('canvas')[0],
-x = c.getContext('2d'),
-pr = window.devicePixelRatio || 1, // High DPI scaling
-w = window.innerWidth,
-h = window.innerHeight,
-points = []
-
-c.width = w*pr;
-c.height = h*pr;
-x.scale(pr, pr);
-
 class Point {
     constructor(x, y){
         this.x = x;
@@ -22,9 +6,7 @@ class Point {
 }
 
 class Curve {
-    constructor(canvas, color, leftPoint, rightPoint, decay, decayRate, minX, minY) {
-        this.canvas2D = canvas.getContext('2d');
-
+    constructor(color, leftPoint, rightPoint, decay, decayRate, minX, minY) {
         this.color = color;
 
         this.leftPoint  = leftPoint;
@@ -65,20 +47,20 @@ class Curve {
         this.generateMidpoint(mid, right, scale + this.decayRate);
     }
 
-    render() {
+    render(context) {
         this.points.forEach((obj, index) => {
             if (index == 0){
-                this.canvas2D.beginPath();
-                this.canvas2D.moveTo(obj.x, obj.y);
+                context.beginPath();
+                context.moveTo(obj.x, obj.y);
             } else {
-                this.canvas2D.lineTo(obj.x, obj.y);
+                context.lineTo(obj.x, obj.y);
             }
         });
-        this.canvas2D.lineTo(this.w, this.h);
-        this.canvas2D.lineTo(0, this.h);
-        this.canvas2D.closePath();
-        this.canvas2D.fillStyle = this.color;
-        this.canvas2D.fill(); 
+        context.lineTo(this.w, this.h);   // bottom right
+        context.lineTo(0, this.h);        // bototm left
+        context.closePath();
+        context.fillStyle = this.color;
+        context.fill(); 
     }
 
     randomY(midY, scale) {
@@ -95,13 +77,26 @@ class Curve {
 class Terrain {
     constructor(canvas, backgroundColor, curves) {
         this.canvas             = canvas;
+        this.canvasContext      = this.canvas.getContext('2d');
+
         this.backgroundColor    = backgroundColor;
         this.curves             = curves;               // Background first, foreground last
 
         this.w                  = window.innerWidth;
         this.h                  = window.innerHeight;
-
+        
+        this.setupCanvas();
         this.generateTerrain();
+    }
+
+    setupCanvas(){
+        const pr = window.devicePixelRatio || 1; // High DPI scaling
+        const w = window.innerWidth;
+        const h = window.innerHeight;
+
+        this.canvas.width = window.innerWidth*pr;
+        this.canvas.height = window.innerHeight*pr;
+        this.canvasContext.scale(pr, pr);
     }
 
     generateTerrain(){
@@ -110,97 +105,56 @@ class Terrain {
     }
 
     setBackground() {
-        this.canvas.fillStyle = this.backgroundColor;
-        this.canvas.fillRect(0, 0, this.w, this.h);
+        this.canvasContext.fillStyle = this.backgroundColor;
+        this.canvasContext.fillRect(0, 0, window.innerWidth, window.innerHeight);
     }
 
     drawCurves() {
-        this.curves.forEach((curve) => curve.render());
+        this.curves.forEach((curve) => curve.render(this.canvasContext));
     }
 }
 
-function generateMidpoint(a, b, scale){
-    var midX = (a.x + b.x)/2;
-    var midY = (a.y + b.y)/2;
-    var mid = new Point(midX, randomY(midY, scale));
-    var dist = mid.x - a.x;
 
-    if (dist <= MIN_X){
-        points.push(a);
-        return;
-    }
-    generateMidpoint(a, mid, scale + DECAY_RATE);
-    generateMidpoint(mid, b, scale + DECAY_RATE);
-}
+const MIN_X = 3,
+MIN_Y = 1000,
+DECAY = 10,
+DECAY_RATE = 20;
 
-function midpointDisplacement(a, b, scale){
-    generateMidpoint(a, b, scale);
-    points.push(b);
-}
-
-function randomY(midY, scale){ // Generate a Y coordinate
-    var random = Math.random() * MIN_Y/scale;
-    var f = Math.floor(random);
-    return (midY + (f * plusOrMinus()));
-}
-
-function plusOrMinus() { // Randomly returns -1 or 1
-    return Math.random() < 0.5 ? -1 : 1;
-}
-
-function drawCurve(color){
-    points.forEach(function(obj, index){
-        if (index == 0){
-            x.beginPath();
-            x.moveTo(obj.x, obj.y);
-        } else {
-            x.lineTo(obj.x, obj.y);
-        }
-    });
-    x.lineTo(w, h);
-    x.lineTo(0, h);
-    x.closePath();
-    x.fillStyle = color;
-    x.fill();    
-}
-
-function setBackground(color){
-    x.fillStyle = color;
-    x.fillRect(0, 0, w, h);
-}
-
-function createTerrain(a, b, color){
-    points = []
-    midpointDisplacement(a, b, DECAY);
-    drawCurve(color);
-}
+const c = document.getElementsByTagName('canvas')[0];
+const w = window.innerWidth;
+const h = window.innerHeight;
 
 let curves = [];
+
 curves.push(new Curve(
-    c,
-    '#9ef95e',
-    new Point(0, 400),
+    '#ebf6ff',
+    new Point(0, 300),
     new Point(w, 200),
+    DECAY,
+    DECAY_RATE + 20,
+    MIN_X,
+    MIN_Y
+))
+
+curves.push(new Curve(
+    '#d9eeff',
+    new Point(0, 400),
+    new Point(w, 300),
+    DECAY,
+    DECAY_RATE + 10,
+    MIN_X,
+    MIN_Y
+));
+
+curves.push(
+new Curve(
+    '#b3ddff',
+    new Point(0, 500),
+    new Point(w, 300),
     DECAY,
     DECAY_RATE,
     MIN_X,
     MIN_Y
-).render())
+));
 
-// // Earth
-// setBackground('#6da5ff');
-// createTerrain(
-//     new Point(0, 500),
-//     new Point(w, 300),
-//     '#9ef95e'
-// );
-// createTerrain(
-//     new Point(0, 500),
-//     new Point(w, 700),    
-//     '#6cb737'
-// );
-// createTerrain(
-//     new Point(0, 800),
-//     new Point(w, 500),
-//     '#3a7a0d'
-// );
+let terrain = new Terrain(c, 'white', curves);
